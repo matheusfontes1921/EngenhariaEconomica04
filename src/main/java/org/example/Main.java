160d2e95a642db426c626ac7da3b71f08861e4d9
matheusfontes1921 <matheusfontes1921@gmail.com>
2024-03-01 19:45:15 -0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        //comentando
+
+
+        /*comenta*/
         System.out.println("Matheus Fontes Almeida Moreira Silva - Engenharia Economica para software");
     }
 }
